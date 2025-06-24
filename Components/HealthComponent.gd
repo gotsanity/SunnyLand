d6b50e99b22fb09c5ccf02424f35655b0c1874d3
gotsanity <gotsanity@gmail.com>
2025-06-24 12:47:14 -0700

    updated health component to be more robust

--- Components/HealthComponent.gd
+++ Components/HealthComponent.gd
@@ -3,12 +3,15 @@ class_name HealthComponent
 
 @export var max_health := 1.0
 var health: float
+@export var hitbox: Hitbox
 
 signal health_changed
 signal died
 
 
 func _ready():
+	if !hitbox:
+		hitbox = get_parent().get_node("Hitbox")
 	health = max_health
 
 
@@ -21,7 +24,8 @@ func damage(attack: Attack):
 	print(get_parent().name + " has been attacked for: " + str(attack.attack_damage).pad_decimals(2))
 	health -= clampf(attack.attack_damage, 0, max_health)
 	health_changed.emit(health)
-	get_parent().get_node("Hitbox").hit_react.emit(attack)
+	if hitbox:
+		hitbox.hit_react.emit(attack)
 	
 	if health <= 0:
 		owner.is_dead = true
