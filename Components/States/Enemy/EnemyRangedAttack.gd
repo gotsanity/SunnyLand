5f176488a73b53cdba55957fcb1177485ecb9895
gotsanity <gotsanity@gmail.com>
2025-06-24 16:22:34 -0700

    feat: Added fireball

--- Components/States/Enemy/EnemyRangedAttack.gd
+++ Components/States/Enemy/EnemyRangedAttack.gd
@@ -1,14 +1,30 @@
 extends State
 class_name EnemyRangedAttack
 
+var enemy: BaseEnemy
+#@export var max_range := 10.0
+#@export var cooldown_duration := 3.0
+
+@export var bullet := preload("res://Interactable/fireball/fireball.tscn")
+@export var bullet_point: Node2D
+@export var bullet_impact := preload("res://Interactable/fireball/fireball_explosion.tscn")
+
+var cooldown_remaining := 0.0
+
+var player: Player
+
 func Enter():
-	pass
+	player = get_tree().get_first_node_in_group("Player")
+	enemy = owner
+	enemy.animated_sprite_2d.play("ranged")
+	enemy.animated_sprite_2d.connect("animation_finished", on_animation_finished)
+	var shot = bullet.instantiate()
+	shot.direction = bullet_point.global_position.direction_to(player.global_position)
+	shot.pos = bullet_point.global_position
+	shot.target = player.global_position
+	shot.explosion = bullet_impact
+	get_tree().root.add_child(shot)
 
-func Exit():
-	pass
-
-func Update(_delta: float):
-	pass
-
-func Physics_Update(_delta: float):
+func on_animation_finished():
+	Transitioned.emit(self, "EnemyIdle")
 	pass
---
+++ Interactable/fireball/fireball.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+class_name Fireball
+
+var pos: Vector2
+var target: Vector2
+var rot: float
+var direction: Vector2
+var speed := 100
+@export var attack: Attack
+@export var time_to_live: float = 2.0
+@export var explosion = preload("res://Interactable/fireball/fireball_explosion.tscn")
+
+
+var timer: Timer
+
+func _ready():
+	timer = Timer.new()
+	timer.autostart = true
+	timer.set_one_shot(true)
+	timer.set_wait_time(time_to_live)
+	timer.connect("timeout", on_timeout)
+	timer.start()
+	add_child(timer)
+	
+	global_position = pos
+	look_at(target)
+
+func _physics_process(_delta):
+	velocity = direction * speed
+	move_and_slide()
+
+func explode():
+	var boom = explosion.instantiate()
+	boom.global_position = global_position
+	get_tree().root.add_child(boom)
+	queue_free()
+
+func on_timeout():
+	explode()
+
+func _on_area_2d_body_entered(body):
+	if body is Player and attack.team == "enemy":
+		body.health_component.damage(attack)
+		explode()
+	
+	if body is BaseEnemy and attack.team == "player":
+		body.health_component.damage(attack)
+		explode()
